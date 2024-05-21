0a7ea57aeaf989ac49052b9cb0185e9c5cf89199
יעל אסתר אילוז <326354982@mbydomain>
2024-05-21 16:30:44 +0300

    add getallmanagerorders component and change style of image in branch

---
+++ client/src/Components/Orders/GetAllManagerOrders.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputSwitch } from 'primereact/inputswitch';
+
+import ManagerOrders from "./ManagerOrders"
+import { useGetAllOrdersQuery } from "../../app/orderApiSlice";
+import { Button } from 'primereact/button';
+
+
+export default function RadioButtonRowSelectionDemo() {
+    const [products, setProducts] = useState([]);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [rowClick, setRowClick] = useState(true);
+const { data:orders=[], isLoading, isError, error, isSuccess } = useGetAllOrdersQuery()
+    
+    useEffect(() => {
+        if (isSuccess){
+            //setProducts(orders)
+            console.log(orders);
+        }
+        else
+            console.log("loading");
+
+    }, [isSuccess]);
+
+
+    // useEffect(() => {
+    //     order.getProductsMini().then((data) => setProducts(data));
+    // }, []);
+
+    const selectedOrder=(e)=>{
+        console.log("e.value",e);
+       <ManagerOrders  order={e} />
+      
+    }
+
+   
+
+    return (
+        <div className="card">
+            <div className="flex justify-content-center align-items-center mb-4 gap-2">
+                <InputSwitch inputId="input-rowclick" checked={rowClick} onChange={(e) => setRowClick(e.value)} />
+                <label htmlFor="input-rowclick">Row Click</label>
+            </div>
+            
+            <DataTable value={orders} selectionMode={rowClick ? null : 'radiobutton'} selection={selectedProduct} onSelectionChange={(e) =>{ selectedOrder(e.value) }} dataKey="id" tableStyle={{ minWidth: '50rem' }}>
+                <Column selectionMode="single" headerStyle={{ width: '3rem' }}></Column>
+                <Column field="status" header="סטטוס"></Column>
+                <Column field="address" header="כתובת"></Column>
+                <Column field="date" header="תאריך"></Column>
+                <Column field="sweets" header="מוצרים שהוזמנו"></Column>
+            </DataTable>
+        </div>
+    );
+}
--- client/src/Components/Orders/ManagerOrders.jsx
+++ client/src/Components/Orders/ManagerOrders.jsx
@@ -1,67 +1,72 @@
-import React, { useRef ,useState,useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Stepper } from 'primereact/stepper';
 import { StepperPanel } from 'primereact/stepperpanel';
 import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
 
+import GetAllManagerOrders from './GetAllManagerOrders'
 import { useUpdateStatusMutation } from "../../app/orderApiSlice";
-import { useGetAllOrdersQuery } from "../../app/orderApiSlice"; 
-export default function BasicDemo({order}) {
+import { useGetAllOrdersQuery } from "../../app/orderApiSlice";
+
+export default function BasicDemo({ order }) {
     const stepperRef = useRef(null);
     const [updateStatus] = useUpdateStatusMutation()
-    //const [products, setOrder] = useState([]);
-    const { data:orders=[], isLoading, isError, error, isSuccess } = useGetAllOrdersQuery()
-    // const orderUpdate =  {id:order._id,status:order.status}
-    useEffect(() => {
-        if (isSuccess){
-            //setProducts(orders)
-            console.log(orders);
-        }
-        else
-            console.log("loading");
 
-    }, [isSuccess]);
+    // const { data: orders = [], isLoading, isError, error, isSuccess } = useGetAllOrdersQuery()
+    // useEffect(() => {
+    //     if (isSuccess) {
+    //         //setProducts(orders)
+    //         console.log(orders);
+    //     }
+    //     else
+    //         console.log("loading");
 
-    
+    // }, [isSuccess]);
+
 
     return (
-        <div className="card flex justify-content-center">
-        <Stepper ref={stepperRef} style={{ flexBasis: '50rem' }}>
-            <StepperPanel  header="התקבל">
-                <div className="flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                     
-                        <Button onClick={()=>updateStatus(orders[2]._id)} >ביצוע ההזמנה</Button>
-                        
+        <>
+            <Card title="הזמנות לקוח" style={{ textAlign: 'center', width: '80%', paddingRight: '10%', paddingLeft: '10%', marginLeft: '10%' }}>
+            <div className="card flex justify-content-center">
+                <Stepper ref={stepperRef} style={{ flexBasis: '50rem' }}>
+                    <StepperPanel header="התקבל">
+                        <div className="flex flex-column h-12rem">
+                            <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
+                                <Button onClick={() => updateStatus(order.id)} >ביצוע ההזמנה</Button>
+                            </div>
                         </div>
-                </div>
-                <div className="flex pt-4 justify-content-end">
-                    <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
-                </div>
-            </StepperPanel>
-            <StepperPanel  header="בדרך ללקוח">
-                <div className="flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                    <Button  onClick={()=>updateStatus(orders[2]._id)}>שליח ההזמנה בדרך</Button>
+                        <div className="flex pt-4 justify-content-end">
+                            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
                         </div>
-                </div>
-                <div className="flex pt-4 justify-content-between">
-                    <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                    <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
-                </div>
-            </StepperPanel>
-            <StepperPanel  header="בוצע">
-                <div className="flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                    <Button  onClick={()=>updateStatus(orders[2]._id)}>ההזמנה התקבלה בהצלחה</Button>
+                    </StepperPanel>
+                    <StepperPanel header="בדרך ללקוח">
+                        <div className="flex flex-column h-12rem">
+                            <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
+                                <Button onClick={() => updateStatus(order.id)}>שליח ההזמנה בדרך</Button>
+                            </div>
                         </div>
-                </div>
-                <div className="flex pt-4 justify-content-start">
-                    <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                </div>
-            </StepperPanel>
-        </Stepper>
-        </div>
-    
+                        <div className="flex pt-4 justify-content-between">
+                            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
+                        </div>
+                    </StepperPanel>
+                    <StepperPanel header="בוצע">
+                        <div className="flex flex-column h-12rem">
+                            <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
+                                <Button  >ההזמנה התקבלה בהצלחה</Button>
+                            </div>
+                        </div>
+
+                    </StepperPanel>
+                </Stepper>
+
+            </div>
+            </Card>
+
+            <div>
+                <Card title="הזמנות לקוח" style={{ textAlign: 'center', width: '80%', paddingRight: '10%', paddingLeft: '10%', marginLeft: '10%' }}>
+                    <GetAllManagerOrders />
+                </Card>
+            </div>
+        </>
     )
 }
-         
