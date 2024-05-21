c8dfab8e994accb178151e4097c42ef35b206b8c
elishevaTufik <eli72fik@gmail.com>
2024-05-21 18:12:13 +0300

    design home page

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,11 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './HomePage.css'; 
 
-function Sweet(props) {
+const ImageGallery = ({ }) => {
 
-    return (
-      <div className="Sweet">
-        <br/>
-      </div>
-    );
-  }
-  
-  export default Sweet;
+  const str='images/'
+  const images=[`${str}1.jpg`,`${str}2.jpg`]
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images]);
+
+  return (
+    <div className="image-gallery">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Image ${index + 1}`}
+          className={index === currentIndex ? 'active' : ''}
+          style={{  
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImageGallery;
