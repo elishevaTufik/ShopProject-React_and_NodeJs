8ab434ccf5335422c0ddf3e6b24be5b287104347
elishevaTufik <eli72fik@gmail.com>
2024-05-22 16:49:03 +0300

    href to homePage and notFound

--- client/src/Components/NotFound.jsx
+++ client/src/Components/NotFound.jsx
@@ -5,8 +5,9 @@ function NotFound(props) {
     return (
       <div className="NotFound">
         <br/> 
-        <div style={{fontSize:'80px',textAlign:'center',color:'#ffffff',padding: '5px'}}>
-אופס.... הדף שביקשת לא נמצא 
+        <div style={{fontSize:'80px',textAlign:'center',color:'rgb(163 113 102)',padding: '5px'}}>
+...אופס<br/>
+ הדף שביקשת לא נמצא 
         
         </div>
       </div>
